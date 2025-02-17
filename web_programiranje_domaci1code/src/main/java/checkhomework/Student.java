ffdbfa5740ec5a9347c6e5dbdfb802f0471bdd13
markostojicic03 <mstojicic12923rn@raf.rs>
2025-02-17 16:34:25 +0100

    Ispravljena logika za profesora i dodata za asistenta
    Ostalo je da se isprave eventualne greske i sredi kod.

--- web_programiranje_domaci1code/src/main/java/checkhomework/Student.java
+++ web_programiranje_domaci1code/src/main/java/checkhomework/Student.java
@@ -1,32 +1,48 @@
 package checkhomework;
 
 
+import java.awt.desktop.SystemSleepEvent;
 import java.util.Random;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Student implements Runnable {
     private final int studentId;
     long studentArrivalTimeMill; // u milisekundama je izrazen
+    long startTimeForProfAndAssis;
     long totalTimeForHomeworkChecking;
     int studentHomeworkGrade;
     CyclicBarrier barrier;
+    private final Semaphore semaphore;
+    public static AtomicInteger sumOfAllGrades = new AtomicInteger(0);
+    public static AtomicInteger actualNumberOfGradedStudents = new AtomicInteger(0); // potrebno je da se inkrementuje kada se oceni student(svejedno da li kod profesora ili asistenta)
+    private final Object lock;
 
-
-    public Student(int studentId, long studentArrivalTime, CyclicBarrier barrier) {
+    public Student(int studentId, long studentArrivalTime, CyclicBarrier barrier, long startTimeForProfAndAssis, Semaphore semaphore, Object lock) {
         this.studentId = studentId;
         this.studentArrivalTimeMill = studentArrivalTime;
         this.barrier = barrier;
+        this.startTimeForProfAndAssis = startTimeForProfAndAssis;
+        this.semaphore = semaphore;
+        this.lock = lock;
     }
 
     @Override
     public void run() {
         try {
-            System.out.println("Student " + studentId + " started at " + studentArrivalTimeMill);
-            System.out.println("Student" + studentId +" waiting for professor");
-            barrier.await(); // ceka se dva studenta
-            System.out.println("We are starting our journey with student "+ studentId);
-            professorHomeworkChecking();
+            while (System.currentTimeMillis() < studentArrivalTimeMill) {
+                //System.out.println("Cekamo studenta.");
+
+            }
+            Random profOrAssis = new Random();
+            int profOrAssisFlag = profOrAssis.nextInt(2);
+            if(profOrAssisFlag == 0) {
+                professorHomeworkChecking();
+            }
+            else{
+                assistantHomeworkChecking();
+            }
+
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -34,14 +50,71 @@ public class Student implements Runnable {
     }
 
 
+    // proveriti vremena
     private void professorHomeworkChecking(){
-        this.totalTimeForHomeworkChecking =  ThreadLocalRandom.current().nextLong(500, 1001);
-        this.studentHomeworkGrade =  ThreadLocalRandom.current().nextInt(5, 11);
-        System.out.println("Student " + studentId + " professor grade " + studentHomeworkGrade);
+        try {
+            long checkingStartTime = System.currentTimeMillis();
+         //   System.out.println("Student" + studentId +" waiting for professor");
+            barrier.await(5000, TimeUnit.MILLISECONDS);
+         //   System.out.println("Professor is ready. We are starting our journey with student "+ studentId);
+            semaphore.acquire();
+            this.totalTimeForHomeworkChecking =  ThreadLocalRandom.current().nextLong(500, 1001);
+            long timeLeftForChecking = 5000 - (checkingStartTime - startTimeForProfAndAssis);
+            if(timeLeftForChecking < totalTimeForHomeworkChecking) {
+                System.out.println("Student "+ this.studentId +" is late(5 seconds passed)...PROFA");
+                semaphore.release();
+                return;
+            }
+
+            try {
+                Thread.sleep(totalTimeForHomeworkChecking);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.studentHomeworkGrade =  ThreadLocalRandom.current().nextInt(5, 11);
+            sumOfAllGrades.addAndGet(studentHomeworkGrade);
+            actualNumberOfGradedStudents.incrementAndGet();
+
+            long endTime = System.currentTimeMillis() - startTimeForProfAndAssis;// proveriti gde se koristi
+            System.out.println("Thread: Student " + studentId + " Arrival: " + (studentArrivalTimeMill - startTimeForProfAndAssis) +
+                    " ms Prof: professor TTC: " + totalTimeForHomeworkChecking + " ms, Start: " + checkingStartTime +
+                    " ms Score: " + studentHomeworkGrade);
+            semaphore.release();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            System.out.println("Timeout: Professor waited too long. Student " + studentId + " is leaving.");
+        }
 
     }
 
     private void assistantHomeworkChecking(){
+       // System.out.println("Student" + studentId +" waiting for assistent");
+        synchronized (this.lock){
+           // System.out.println("Assistant is ready. We are starting our journey with student "+ studentId);
+            long checkingStartTime = System.currentTimeMillis();
+            this.totalTimeForHomeworkChecking =  ThreadLocalRandom.current().nextLong(500, 1001);
+            long timeLeftForChecking = 5000 - (System.currentTimeMillis() - startTimeForProfAndAssis);
+            if(timeLeftForChecking < totalTimeForHomeworkChecking) {
+                System.out.println("Student "+ this.studentId +" is late...ASIST");
+                return;
+            }
 
+            try {
+                Thread.sleep(totalTimeForHomeworkChecking);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.studentHomeworkGrade =  ThreadLocalRandom.current().nextInt(5, 11);
+            sumOfAllGrades.addAndGet(studentHomeworkGrade);
+            actualNumberOfGradedStudents.incrementAndGet();
+
+            long endTime = System.currentTimeMillis() - startTimeForProfAndAssis;// proveriti gde se koristi
+            System.out.println("Thread: Student " + studentId + " Arrival: " + (studentArrivalTimeMill - startTimeForProfAndAssis) +
+                    " ms Prof: assistant TTC: " + totalTimeForHomeworkChecking + " ms, Start: " + checkingStartTime +
+                    " ms Score: " + studentHomeworkGrade);
+        }
     }
 }
--- web_programiranje_domaci1code/src/main/java/main/Main.java
+++ web_programiranje_domaci1code/src/main/java/main/Main.java
@@ -3,22 +3,29 @@ package main;
 import checkhomework.Student;
 
 import java.util.Random;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int n = 4; // broj studenata; moze da se promeni da se ucitava sa konzole
+        int n = 20; // broj studenata; moze da se promeni da se ucitava sa konzole
         ExecutorService executorService = Executors.newCachedThreadPool();
         int totalStudentsForProfessor = 2;
         CyclicBarrier barrier = new CyclicBarrier(totalStudentsForProfessor);
-
+        Semaphore semaphore = new Semaphore(totalStudentsForProfessor);
+        final Object lock = "LOCK";
         for (int i = 0; i < n; i++) {
-            Random random = new Random();
-            executorService.execute(new Student(i, random.nextInt(1000), barrier));
+            long studentArrivalTimeMill = ThreadLocalRandom.current().nextInt(1, 1001);
+            long startTimeForProf = System.currentTimeMillis();
+            executorService.execute(new Student(i, studentArrivalTimeMill+startTimeForProf, barrier, startTimeForProf, semaphore, lock));
         }
+        try {
+            Thread.sleep(5000); // Simulacija trajanja odbrana PROVERITI DA LI JE OVO POTREBNO DA OSTANE U OVOM OBLIKU
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+          //  e.printStackTrace();
+        }
+        System.out.println("Average grade: " + Student.sumOfAllGrades.get() * 1.0 / Student.actualNumberOfGradedStudents.get());
         executorService.shutdown();
     }
 }
